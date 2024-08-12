fbab141fd2abe24f7be609676fe041233b1a3006
YUGE0 <yugprajapati32@gmail.com>
2024-08-12 14:28:11 +0530

    Loading screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import { useState,React } from 'react'
+import { useState,React,Suspense,useEffect } from 'react'
 import Time from './Time'
 import Hour from './Hour'
 import Minute from './Minute'
 import Nav from './Nav'
 import FullScreenProvider from './FullScreenContext'
 
+// function Load() {
+//   return <h2>Loading...</h2>;
+// }
+
 
 function App() {
 
-  return (
-    <FullScreenProvider>
-    <div className="px-20 xl:py-20 md:py-22 sm:py-48 pb-40 dark:bg-black h-full">
-      <div>
-        <Nav />
-      </div>
-      <div className="flex justify-evenly flex-wrap">
-        <Hour/>
-        <Minute/>
-      </div>
-      <div className="p-10 flex flex-wrap justify-center">
-        <Time city="New York"/>
-        <Time city="Frankfurt"/>
-        <Time city="Helsinki"/>
-      </div>
-    </div>
-    </FullScreenProvider>
-)
+  const [isLoading, setIsLoading] = useState(true);
+    
+      useEffect(() => {
+        // Simulate an API call
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      }, []);
+    
+      if (isLoading) {
+        return <h1 className="p-40 pl-[22rem] text-[30rem] text-black font-inter font-bold">TIME</h1>
+      }
+      return (
+        <FullScreenProvider>
+        <div className="px-20 xl:py-20 md:py-22 sm:py-48 pb-40 dark:bg-black h-full">
+          <div>
+            <Nav />
+          </div>
+          <div className="flex justify-evenly flex-wrap">
+            <Hour/>
+            <Minute/>
+          </div>
+          <div className="p-10 flex flex-wrap justify-center">
+            <Time city="New York"/>
+            <Time city="Frankfurt"/>
+            <Time city="Helsinki"/>
+          </div>
+        </div>
+        </FullScreenProvider>
+        )
 }
 
 export default App
